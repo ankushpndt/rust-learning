abefb4729e68d85c06449e829f504d14bded0fad
ankush <ankush@compose.co.in>
2024-12-23 18:21:25 +0530

    add: chapter 3 functions

--- chapter_3/src/main.rs
+++ chapter_3/src/main.rs
@@ -1,16 +1,22 @@
-const PI: i32 = 3.14;
+// const PI:f32  = 3.14;
 fn main() {
     //difference between let and const
     //const is always immutable and type needs to be define
     //const can be declared as a global variable
     //difference between mut and shadowing
     //shadowing
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("No of spaces {spaces}");
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+    // println!("No of spaces {spaces}");
 
     //mut
-    let mut spaces = "  ";
-    spaces = spaces.len();
-    println!("No of spaces {spaces}");
+    // let mut spaces = "  ";
+    // spaces = spaces.len();
+    // println!("No of spaces {spaces}");
+    my_func(3);
+}
+
+fn my_func(x: i8) -> i8 {
+    println!("The value is {x}");
+    x
 }
