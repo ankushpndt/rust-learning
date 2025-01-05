8e5afc7c474b6db48a3a8955bf755739b2bed9a2
ankush <ankush@compose.co.in>
2025-01-06 01:15:46 +0530

    add: associated functions and methods

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -5,15 +5,32 @@ struct User {
     sign_in_count: u64,
 }
 
-struct Color(u8,u8,u8);
-struct Point(u8,u8,u8);
+struct Color(u8, u8, u8);
+struct Point(u8, u8, u8);
 
 struct AlwaysEqual; // unit type struct
 
 #[derive(Debug)]
-struct Rectangle{
+struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn calc_area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) ->bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(side: u32) -> Self {
+        Self {
+            width: side,
+            height: side,
+        }
+    }
 }
 
 fn main() {
@@ -38,24 +55,30 @@ fn main() {
     //tuple structs
     let red = Color(100, 0, 0);
     set_bg_color(red);
-    let point = Point(1,1,1);
+    let point = Point(1, 1, 1);
     move_point(point);
     //color can also be passed in move_point bcoz both point and color have same structure so it's difficult to differentiate between them
     //so to make them a different type tuple we use tuple structs
     //move_point(red) is not possible now
 
-
     let rect1 = Rectangle {
         width: 10,
-        height: 10
+        height: 10,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.calc_area()
     );
     println!("{:#?}", rect1);
     dbg!(&rect1); //dbg! takes ownership that's why using the reference of rect1
+    let rect2 = Rectangle {
+        width: 5,
+        height: 5,
+    };
+    println!("Can rect1 hold rect2? {:?}", rect1.can_hold(&rect2));
+    let sq = Rectangle::square(5);
+    dbg!(&sq);
 }
 
 fn build_user(name: String, email: String) -> User {
@@ -75,12 +98,5 @@ fn set_bg_color(color: Color) {
 }
 
 fn move_point(point: Point) {
-    println!(
-        "Point is X={}, Y={}, Z={}",
-        point.0, point.1, point.2
-    )
-}
-
-fn area(rect1: &Rectangle) -> u32 {
-    rect1.width * rect1.height
+    println!("Point is X={}, Y={}, Z={}", point.0, point.1, point.2)
 }
